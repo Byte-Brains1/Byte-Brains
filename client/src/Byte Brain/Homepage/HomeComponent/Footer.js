f575f3a998cf02a9c5513fcd0ea60c27eade3d3d
Byte-Brains1 <bytebrainsweb@gmail.com>
2025-01-12 06:58:36 +0530

    Update Footer.js

--- client/src/Byte Brain/Homepage/HomeComponent/Footer.js
+++ client/src/Byte Brain/Homepage/HomeComponent/Footer.js
@@ -8,13 +8,10 @@ const Footer = () => {
             <div className="footer-container">
                 <h2>Contact Us</h2>
                 <div className="social-links">
-                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+                    <a href="https://www.instagram.com/byte_brains_club/profilecard/?igsh=MTkzdGt5amM3ajh5aw== " target="_blank" rel="noopener noreferrer" className="social-icon">
                         <img src="./logo/Instagram.png" alt="Instagram" className="social-logo" />
                     </a>
-                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="social-icon">
-                        <img src="./logo/Linkedin.png" alt="LinkedIn" className="social-logo" />
-                    </a>
-                    <a href="mailto:bytebrains@gmail.com" target="_blank" className="social-icon" rel="noopener noreferrer">
+                    <a href="mailto:bytebrains@gmail.com" className="social-icon">
                         <img src="./logo/gmail.png" alt="Gmail" className="social-logo" />
                     </a>
                 </div>
